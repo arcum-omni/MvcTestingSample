db3dce9512e9997b4e006b1926a8e7b014f64082
arcum-omni <travis.l.eiler@gmail.com>
2020-05-06 10:26:17 -0700

    Add project files.

---
+++ MvcTestingSampleTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcTestingSample.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvcTestingSample.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
+namespace MvcTestingSampleTests.Controllers
+{
+    class HomeControllerTests
+    {
+    }
+}
