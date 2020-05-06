93dea7e2ca68ea420da4f64792bedec3c7f51228
arcum-omni <travis.l.eiler@gmail.com>
2020-05-06 12:49:34 -0700

    Created blank interface IProductRepository

---
+++ MvcTestingSample/Models/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcTestingSample.Models.Interfaces
+{
+    interface IProductRepository
+    {
+        /*
+            stopped video @ 4:06
+            https://cptc.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=e842825d-7a10-46dc-b461-abb300634134&start=undefined
+        */
+    }
+}
