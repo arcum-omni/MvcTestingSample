a487cd41fb783273f70b017f23abf3c35894cdfa
arcum-omni <travis.l.eiler@gmail.com>
2020-05-06 11:05:09 -0700

    Overloaded HomeController() for no arg

--- MvcTestingSample/Controllers/HomeController.cs
+++ MvcTestingSample/Controllers/HomeController.cs
@@ -13,6 +13,10 @@ namespace MvcTestingSample.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        public HomeController()
+        {
+        }
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
