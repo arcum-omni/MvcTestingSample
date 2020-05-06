b94a005e320505d964fbcac5e68edbc43462b138
arcum-omni <travis.l.eiler@gmail.com>
2020-05-06 12:43:16 -0700

    Added product class

---
+++ MvcTestingSample/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcTestingSample.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductPrice { get; set; }
+    }
+}
