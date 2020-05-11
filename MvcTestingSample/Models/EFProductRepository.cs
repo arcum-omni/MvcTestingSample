09000b375cda229e5283e782b215c131de83a0f8
arcum-omni <travis.l.eiler@gmail.com>
2020-05-11 07:38:18 -0700

    Created  product repo and default implementation

---
+++ MvcTestingSample/Models/EFProductRepository.cs
@@ -0,0 +1,39 @@
+﻿using MvcTestingSample.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcTestingSample.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        // Application DB Context
+
+
+        public Task AddProductAsync(Product p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProductAsync(Product p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProductAsync(Product p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MvcTestingSample/Models/Interfaces/IProductRepository.cs
+++ MvcTestingSample/Models/Interfaces/IProductRepository.cs
@@ -11,5 +11,17 @@ namespace MvcTestingSample.Models.Interfaces
             stopped video @ 4:06
             https://cptc.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=e842825d-7a10-46dc-b461-abb300634134&start=undefined
         */
+
+        Task<Product> GetProductByIdAsync(int id); // public is unneccessary
+
+        // Retrieve Products
+        // Could include parameters for pagination GetAllProductsAsync(int pageNum, ing pageSize);
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+
+        Task AddProductAsync(Product p);
+
+        Task UpdateProductAsync(Product p);
+
+        Task DeleteProductAsync(Product p);
     }
 }
