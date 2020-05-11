cf20ec01874980bfc768319eea7289fb6f3d3620
arcum-omni <travis.l.eiler@gmail.com>
2020-05-11 07:37:27 -0700

    Created DB Context

---
+++ MvcTestingSample/Models/ProductDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcTestingSample.Models
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
