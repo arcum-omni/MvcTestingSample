4f05ea206f124a497c2f7822d000a6638c91a18d
arcum-omni <travis.l.eiler@gmail.com>
2020-05-13 09:34:21 -0700

    Created products controller

---
+++ MvcTestingSample/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcTestingSample.Models;
+using MvcTestingSample.Models.Interfaces;
+
+namespace MvcTestingSample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _repo;
+
+        public ProductsController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // TODO: Resume video W6D1.1@14:25
+
+            List<Product> products = await _repo.GetAllProductsAsync();
+            return View(products);
+        }
+    }
+}
