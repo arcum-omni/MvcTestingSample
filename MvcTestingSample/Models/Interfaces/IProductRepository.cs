2431c9cf4fbe71686d68f1f11defe6b4036a353d
arcum-omni <travis.l.eiler@gmail.com>
2020-05-13 09:23:39 -0700

    changed container type to List, removed old comment

--- MvcTestingSample/Models/Interfaces/IProductRepository.cs
+++ MvcTestingSample/Models/Interfaces/IProductRepository.cs
@@ -5,18 +5,13 @@ using System.Threading.Tasks;
 
 namespace MvcTestingSample.Models.Interfaces
 {
-    interface IProductRepository
+    public interface IProductRepository
     {
-        /*
-            stopped video @ 4:06
-            https://cptc.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=e842825d-7a10-46dc-b461-abb300634134&start=undefined
-        */
-
         Task<Product> GetProductByIdAsync(int id); // public is unneccessary
 
         // Retrieve Products
         // Could include parameters for pagination GetAllProductsAsync(int pageNum, ing pageSize);
-        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<List<Product>> GetAllProductsAsync();
 
         Task AddProductAsync(Product p);
 
