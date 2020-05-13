45aecc1141856b636a2f259f1f6ef3cc592b207b
arcum-omni <travis.l.eiler@gmail.com>
2020-05-13 12:36:37 -0700

    Added unit test for product controller

---
+++ MvcTestingSampleTests/Controllers/ProductsControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MvcTestingSample.Controllers;
+using MvcTestingSample.Models;
+using MvcTestingSample.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcTestingSample.Controllers.Tests
+{
+    [TestClass()]
+    public class ProductsControllerTests
+    {
+        [TestMethod()]
+        public async Task Index_ReturnsViewResult_WithListOfAllProducts()
+        {
+            // *** Arrange *** //
+            Mock<IProductRepository> mockRepo = new Mock<IProductRepository>();
+            mockRepo.Setup(repo => repo.GetAllProductsAsync()).ReturnsAsync(GetProducts());
+
+            var prodController = new ProductsController(mockRepo.Object);
+
+            // *** Act     *** //
+            IActionResult result = await prodController.Index();
+
+            // *** Assert  *** //
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        private List<Product> GetProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    ProductID = 1, ProductName = "Computer", ProductPrice = "1999.99"
+                },
+                new Product()
+                {
+                    ProductID = 2, ProductName = "Webcam", ProductPrice = "199.99"
+                },
+                new Product()
+                {
+                    ProductID = 3, ProductName = "Desk", ProductPrice = "299.99"
+                },
+                new Product()
+                {
+                    ProductID = 4, ProductName = "Linux Server", ProductPrice = "2999.99"
+                }
+            };
+        }
+    }
+}
