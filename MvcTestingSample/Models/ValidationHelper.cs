647658b2e15a4e25f5068cdafe4d93f13a79a8d1
arcum-omni <travis.l.eiler@gmail.com>
2020-05-18 10:30:03 -0700

    Added property & static method tests

---
+++ MvcTestingSample/Models/ValidationHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MvcTestingSample.Models
+{
+    public static class ValidationHelper
+    {
+        public static bool IsValidPrice(string price)
+        {
+            if (price == string.Empty)
+            {
+                return false;
+            }
+
+            // ^\$?\d{0,}\.?(\d{1,})?$
+            // ^(\$)?([1-9]{1}[0-9]{0,2})(\,\d{3})*(\.\d{2})?$|^(\$)?([1-9]{1}[0-9]{0,2})(\d{3})*(\.\d{2})?$|^(0)?(\.\d{2})?$|^(\$0)?(\.\d{2})?$|^(\$\.)(\d{2})?$
+            Regex pattern = new Regex(@"^\$?\d{0,}\.?(\d{1,})?$");
+            return pattern.IsMatch(price);
+
+            //try
+            //{
+            //    Convert.ToDouble(price);
+            //    return true;
+            //}
+            //catch (FormatException)
+            //{
+            //    return false;
+            //}
+        }
+    }
+}
---
+++ MvcTestingSampleTests/Models/ValidationHelperTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcTestingSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvcTestingSample.Models.Tests
+{
+    [TestClass()]
+    public class ValidationHelperTests
+    {
+        [TestMethod()]
+        [DataRow("9.99")]
+        [DataRow("$9.99")]
+        [DataRow(".99")]
+        [DataRow("0.99")]
+        [DataRow("0")]
+        public void IsValidPriceTest_ValidPrice_ReturnsTrue(string input)
+        {
+            bool result = ValidationHelper.IsValidPrice(input);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        [DataRow("")]
+        [DataRow(" ")]
+        [DataRow("nine")]
+        [DataRow("9.99.99")]
+        [DataRow("9.99$")]
+        public void IsValidPriceTest_InvalidPrice_ReturnFalse(string input)
+        {
+            bool result = ValidationHelper.IsValidPrice(input);
+            Assert.IsFalse(result);
+        }
+    }
+}
